9fd4edff164a6e9501dbe96d5b55e9a4fc940ad0
CodeMachan <mdpkcse@gmail.com>
2025-09-28 08:15:46 -0400

    Merge2SortedLists

---
+++ LinkedList/Merge2SortedList.cs
@@ -0,0 +1,57 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/?envType=problem-list-v2&envId=linked-list
+// Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Merge2SortedList
+{
+    public Merge2SortedList()
+    {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        var result = MergeTwoLists(list1, list2);
+        PrintList(result);
+    }
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new ListNode(-1);
+        var current = dummy;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null)
+            current.next = list1;
+        else
+            current.next = list2;
+
+        return dummy.next;
+
+    }
+    public static void PrintList(ListNode head) {
+    while (head != null) {
+        Console.Write(head.val + " ");
+        head = head.next;
+    }
+    Console.WriteLine();
+}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // https://leetcode.com/problems/merge-two-sorted-lists/?envType=problem-list-v2&envId=linked-list
+        Merge2SortedList sol = new Merge2SortedList();
+        
+        
+        
+    }
+}
+
